aa2bc0f9e5ab187ca66645ac0470e6631c28ebe8
Marvin <dermarvin.mobil@gmail.com>
2018-11-07 00:15:56 +0100

    update: use travis for CNAME

--- publish.ps1
+++ publish.ps1
@@ -2,13 +2,14 @@ iex 'cd C:\Users\marvin\PycharmProjects\admindojo-website'
 iex '.\venv\Scripts\activate'
 Remove-Item docs -Force -Recurse
 New-Item -ItemType directory -Path docs
-$generate = 'docnado --html docs --nav_limit taskbutler'
+$env:DN_DOCS_DIR = "pages"
+$generate = 'docnado --html docs --nav_limit taskbutler,bashblog,admindojo'
 iex $generate
 $redirect = @"
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="refresh" content="0; url=./w/index.html">
+        <meta http-equiv="refresh" content="0; url=./w/">
     </head>
 <body>
 </body>
@@ -16,7 +17,9 @@ $redirect = @"
 "@
 
 Set-Content -Value $redirect -Path docs\index.html
-
-
+iex 'cd docs'
+iex 'git add .'
+iex 'cd ..'
+iex 'git commit -m "Site update"'
 iex 'git subtree split --branch gh-pages --prefix docs/'
 #Remove-Item docs -Force -Recurse
